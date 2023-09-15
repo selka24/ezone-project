05f901c85deeff4a688379782e5ee5a222c733ca
selka24 <92512624+selka24@users.noreply.github.com>
2023-09-15 10:53:18 +0200

    debug

--- middleware/business.ts
+++ middleware/business.ts
@@ -1,6 +1,9 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     const user = useSupabaseUser();
-    console.log(user, 'useriiiiiii')
+    console.log(user, 'user main var');
+    console.log(user?.value, 'user value');
+    console.log(user?.value?.id, 'user id');
+
     // if (!user?.value) return navigateTo('/redirect')
 })
   
