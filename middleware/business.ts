270b756c0d57faeebe9a8877160767c7669839b3
selka24 <92512624+selka24@users.noreply.github.com>
2023-09-15 12:03:36 +0200

    testing netlify redirection

--- middleware/business.ts
+++ middleware/business.ts
@@ -3,7 +3,8 @@ export default defineNuxtRouteMiddleware((to, from) => {
     console.log(user, 'user main var');
     console.log(user?.value, 'user value');
     console.log(user?.value?.id, 'user id');
-    if(!user){
+    if(!(user?.value)){
+        return navigateTo('/redirect')
         console.log('should redirect user');
     } else {
         console.log('should allow user', user);
