b4fed3bc71880de6a02b23802edadb86f0ced131
selka <gled907@gmail.com>
2023-11-14 13:09:11 +0100

    changed scrolling

---
+++ components/booking/CompanyLogo.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const mainStore = useMainStore();
+const {public: {logoUrl}} = useRuntimeConfig();
+
+const bookingCompany = computed(() => {
+    return mainStore.businessInfo
+})
+</script>
+
+<template>
+    <div class="flex flex-col justify-between items-center gap-x-5 gap-y-2">
+        <!--                    <h3 class="card-title">-->
+        <!--                        {{bookingCompany.name}}-->
+        <!--                    </h3>-->
+        <nuxt-img
+            format="webp"
+            width="150"
+            height="150"
+            class="w-auto h-[150px] rounded-full"
+            :src="`${logoUrl}/${bookingCompany.logo_url}`"
+            alt="business-logo"/>
+        <!--                    <p>{{ bookingCompany.description }}</p>-->
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- pages/user/[username].vue
+++ pages/user/[username].vue
@@ -1,122 +1,86 @@
 <template>
-    <div class="px-5 flex flex-col items-center justify-center mt-5">
-<!--        <nuxt-img-->
-<!--            format="webp"-->
-<!--            width="300"-->
-<!--            height="100"-->
-<!--            src="/images/logo.png"-->
-<!--            alt="e zone logo"-->
-<!--        />-->
-        <div v-if="bookingCompany?.id" class="card items-center w-full max-w-lg bg-base-100 shadow-2xl">
-            <div class="card-body w-full overflow-hidden">
-                <div class="flex flex-col justify-between items-center gap-x-5 gap-y-2">
-<!--                    <h3 class="card-title">-->
-<!--                        {{bookingCompany.name}}-->
-<!--                    </h3>-->
-                    <nuxt-img
-                        format="webp"
-                        width="150"
-                        height="150"
-                        class="w-auto h-[150px] rounded-full"
-                        :src="`${logoUrl}/${bookingCompany.logo_url}`"
-                        alt="business-logo"/>
-<!--                    <p>{{ bookingCompany.description }}</p>-->
-                </div>
-                <div class="w-full text-left flex flex-col">
-<!--                    <div class="flex flex-grow-0 flex-shrink mb-5 border-b border-secondary-content/20 pb-4">-->
-<!--                        <div class="relative flex w-full">-->
-<!--                            <Icon name="ion:arrow-back-outline" class="cursor-pointer absolute left-0 top-1/2 -translate-y-1/2" @click="changeStep(0)"/>-->
-<!--                            <transition mode="out-in">-->
-<!--                                <div :key="bookStep" class="font-medium">-->
-<!--                                    {{steps[bookStep]}}-->
-<!--                                </div>-->
-<!--                            </transition>-->
-<!--                        </div>-->
-<!--                    </div>-->
-                    <div class="flex flex-auto relative border-secondary-content/20 mb-5 overflow-hidden overflow-y-scroll no-scrollbar h-[400px]" id="parent-steps">
-                        <div class="w-full">
-<!--                            <Transition name="slide-fade" mode="out-in">-->
-                            <div class="h-[400px]" id="stepper-1">
-                                <div class="flex mb-5 border-b border-secondary-content/20 pb-4">
-                                    {{steps[1]}}
-                                </div>
-                                <service-select />
-                            </div>
-                                <!--                            - {{format(bookingStore.selectedDate, 'EEEE d LLLL')}}-->
-                            <div v-show="!!availableSteps.find(x => x === 2)" class="h-[400px]" id="stepper-2">
-                                <div class="flex my-5 border-b border-secondary-content/20 pb-4">
-                                    {{steps[2]}}
-                                </div>
-                                <booking-calendar />
-                            </div>
-                            <div class="h-[400px]" id="stepper-3" v-show="!!availableSteps.find(x => x === 3)">
-                                <div class="flex my-5 border-b border-secondary-content/20 pb-4">
-                                    {{steps[3]}}
-                                </div>
-                                <div class="flex flex-col gap-5 overflow-scroll no-scrollbar h-[350px]">
-                                    <div :class="['form-control px-3 border border-gray-200 rounded-[20px] h-[80px] flex', {'bg-primary border-primary text-white': bookingStore.selectedTime === time}]"
-                                         v-for="time in bookingStore.availableTimes"
-                                         :key="time.clock">
-                                        <label :for="time.clock" class="flex label cursor-pointer">
-                                            <span class="font-semibold text-center">{{ time.clock.split('-')[0] }}</span>
-                                            <span class="flex text-sm text-center gap-5">
-                                                <span v-for="e in time.availableEmpl" class="">
-<!--                                                    {{e}}-->
-                                                    {{mainStore.businessEmployees.find(bE => bE.user_id === e).name}}
-                                                </span>
-                                            </span>
-<!--                                            <span class="text-sm text-center">{{ time.split('-')[1] }}</span>-->
-                                            <input :id="time.clock"
-                                                   type="radio"
-                                                   name="times"
-                                                   :value="time"
-                                                   v-model="bookingStore.selectedTime"
-                                                   class="hidden radio radio-primary"
-                                            />
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="h-[400px]" id="stepper-4" v-show="!!availableSteps.find(x => x === 4)">
-                                <div class="flex my-5 border-b border-secondary-content/20 pb-4">
-                                    {{steps[4]}}
-                                </div>
-                                <div class="flex flex-col gap-3 mb-4">
-                                    <div class="flex gap-2 items-center">
-                                        <span class="label-text">Sherbimi:</span> <div class="font-semibold" v-for="sr in bookingStore.selectedService" :key="sr.id + 'sr'">{{sr.name}}</div>
-                                    </div>
-                                    <div class="flex gap-2 items-center">
-                                        <span class="label-text">Data:</span> <div class="font-semibold" v-if="bookingStore.selectedDate">{{format(bookingStore.selectedDate, 'dd/LL/yyyy')}} - {{bookingStore.selectedTime}}</div>
-                                    </div>
-                                </div>
-                                <div class="flex gap-2 mb-2">
-                                    <div class="form-control w-full max-w-xs">
-                                        <label for="emri" class="label">
-                                            <span class="label-text">Emri*</span>
-                                        </label>
-                                        <input v-model="bookingStore.bookName" id="emri" type="text" placeholder="Emri juaj" class="input input-bordered w-full max-w-xs" />
-                                    </div>
-                                    <div class="form-control w-full max-w-xs">
-                                        <label for="numri" class="label">
-                                            <span class="label-text">Nr. celular*</span>
-                                        </label>
-                                        <input v-model="bookingStore.bookNumber" id="numri" type="text" placeholder="Numri i telefonit" class="input input-bordered w-full max-w-xs" />
-                                    </div>
-                                </div>
-                                <div class="form-control">
-                                    <label for="shenime" class="label">
-                                        <span class="label-text">Shenime</span>
-                                    </label>
-                                    <textarea id="shenime" class="textarea textarea-bordered h-24" placeholder="Vendos shenime"></textarea>
-                                </div>
+    <div class="flex flex-col items-center justify-center h-[100vh]">
+        <div v-if="bookingCompany?.id" class="w-full max-w-lg bg-base-100 h-[95vh] max-h-[800px] flex flex-col">
+            <CompanyLogo/>
+            <div class="w-full relative flex-1 overflow-hidden overflow-y-auto no-scrollbar">
+                <transition-group name="scroll">
+                    <div v-if="bookStep === 1" class="absolute h-[100%] w-full">
+                        <div class="flex mb-5 border-b border-secondary-content/20 pb-4">
+                            {{steps[1]}}
+                        </div>
+                        <service-select />
+                    </div>
+                    <div v-else-if="bookStep === 2" class="absolute h-[100%] w-full">
+                        <div class="flex my-5 border-b border-secondary-content/20 pb-4">
+                            {{steps[2]}}
+                        </div>
+                        <booking-calendar />
+                    </div>
+                    <div v-else-if="bookStep === 3" class="absolute h-[100%] w-full">
+                        <div class="flex my-5 border-b border-secondary-content/20 pb-4">
+                            {{steps[3]}}
+                        </div>
+                        <div class="flex flex-col gap-5 overflow-scroll no-scrollbar h-[400px]">
+                            <div :class="['form-control px-3 border border-gray-200 rounded-[20px] h-[80px] flex', {'bg-primary border-primary text-white': bookingStore.selectedTime === time}]"
+                                 v-for="time in bookingStore.availableTimes"
+                                 :key="time.clock">
+                                <label :for="time.clock" class="flex label cursor-pointer">
+                                    <span class="font-semibold text-center">{{ time.clock.split('-')[0] }}</span>
+                                    <span class="flex text-sm text-center gap-5">
+                                    <span v-for="e in time.availableEmpl" class="">
+                                        {{mainStore.businessEmployees.find(bE => bE.user_id === e).name}}
+                                    </span>
+                                </span>
+                                    <!--                                            <span class="text-sm text-center">{{ time.split('-')[1] }}</span>-->
+                                    <input :id="time.clock"
+                                           type="radio"
+                                           name="times"
+                                           :value="time"
+                                           v-model="bookingStore.selectedTime"
+                                           class="hidden radio radio-primary"
+                                    />
+                                </label>
                             </div>
                         </div>
                     </div>
-                    <div class="flex w-full bg-base-100 justify-center items-center">
-                        <button :disabled="stepInvalid" @click="changeStep" class="btn btn-primary">{{ bookStep === 4 ? 'Prenoto' : 'Vazhdo' }}</button>
-                        <button @click="handleGetBookingsByDate" class="btn btn-primary">TEST</button>
+                    <div v-else-if="bookStep === 4" class="absolute px-5 h-[100%] w-full" id="stepper-4">
+                        <div class="flex my-5 border-b border-secondary-content/20 pb-4">
+                            {{steps[4]}}
+                        </div>
+                        <div class="flex flex-col gap-3 mb-4">
+                            <div class="flex gap-2 items-center">
+                                <span class="label-text">Sherbimi:</span> <div class="font-semibold" v-for="sr in bookingStore.selectedService" :key="sr.id + 'sr'">{{sr.name}}</div>
+                            </div>
+                            <div class="flex gap-2 items-center">
+                                <span class="label-text">Data:</span> <div class="font-semibold" v-if="bookingStore.selectedDate">{{format(bookingStore.selectedDate, 'dd/LL/yyyy')}}, {{bookingStore.selectedTime.clock}}</div>
+                            </div>
+                        </div>
+                        <div class="flex gap-2 mb-2">
+                            <div class="form-control w-full max-w-xs">
+                                <label for="emri" class="label">
+                                    <span class="label-text">Emri*</span>
+                                </label>
+                                <input v-model="bookingStore.bookName" id="emri" type="text" placeholder="Emri juaj" class="input input-bordered w-full max-w-xs" />
+                            </div>
+                            <div class="form-control w-full max-w-xs">
+                                <label for="numri" class="label">
+                                    <span class="label-text">Nr. celular*</span>
+                                </label>
+                                <input v-model="bookingStore.bookNumber" id="numri" type="text" placeholder="Numri i telefonit" class="input input-bordered w-full max-w-xs" />
+                            </div>
+                        </div>
+                        <div class="form-control">
+                            <label for="shenime" class="label">
+                                <span class="label-text">Shenime</span>
+                            </label>
+                            <textarea id="shenime" class="textarea textarea-bordered h-24" placeholder="Vendos shenime"></textarea>
+                        </div>
                     </div>
-                </div>
+                </transition-group>
+            </div>
+            <div class="flex flex-shrink w-full bg-base-100 justify-center items-center">
+                <button :disabled="stepInvalid" @click="changeStep" class="btn btn-primary">{{ bookStep === 4 ? 'Prenoto' : 'Vazhdo' }}</button>
+<!--                <button @click="handleGetBookingsByDate" class="btn btn-primary">TEST</button>-->
             </div>
         </div>
         <company-card-skeleton v-else />
@@ -129,6 +93,7 @@
     import ServiceSelect from "~/components/booking/ServiceSelect.vue";
     import CompanyCardSkeleton from "~/components/loading/CompanyCardSkeleton.vue";
     import {useUtils} from "~/composables/utils";
+    import CompanyLogo from "~/components/booking/CompanyLogo.vue";
     const {public: {logoUrl}} = useRuntimeConfig();
     const bookStep = ref(1);
     const route = useRoute();
@@ -182,9 +147,9 @@
             availableSteps.value = [...availableSteps.value, nextStep];
         }
         bookStep.value = nextStep;
-        nextTick(() => {
-            useScrollTo('parent-steps', `stepper-${bookStep.value}`);
-        })
+        // nextTick(() => {
+        //     useScrollTo('parent-steps', `stepper-${bookStep.value}`);
+        // })
         if(nextStep === 3){
             handleGetBookingsByDate();
         }
