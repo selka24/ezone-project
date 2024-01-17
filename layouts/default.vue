6a1fcec0136a565cc0ce5c0ecf979886c45c619b
selka24 <92512624+selka24@users.noreply.github.com>
2024-01-17 09:03:55 +0100

    admin sidebar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,15 @@
 <template>
     <div>
-        <lazy-header v-if="user && !isOnUserPath"/>
-        <slot/>
+<!--        <lazy-header v-if="user && !isOnUserPath"/>-->
+        <sidebar v-if="user && !isOnUserPath"/>
+        <div v-if="user && !isOnUserPath" class="ml-16">
+            <slot/>
+        </div>
     </div>
 </template>
 <script setup>
+    import Sidebar from "~/components/admin/Sidebar.vue";
+
     const user = useSupabaseUser();
     const isOnUserPath = useRoute().path.includes('user');
 </script>
